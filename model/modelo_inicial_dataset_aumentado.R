24feafaac0c066ebd43b2ea86055c1b4106c441f
mcnanton <42379883+mcnanton@users.noreply.github.com>
2022-12-01 23:39:49 -0300

    Add codigo para explainers globales + shap

--- model/modelo_inicial_dataset_aumentado.R
+++ model/modelo_inicial_dataset_aumentado.R
@@ -5,6 +5,7 @@ library(rsample)
 library(bonsai)
 library(DALEXtra)
 library(themis)
+
 tidymodels_prefer()
 
 set.seed(1234)
@@ -189,7 +190,30 @@ full_dataset <-
   recipe(AS ~ ., 
          data = dataset)
 
-testing <- last_fit(lgbm_final, full_dataset, split = dataset_split) 
+best_params_manual <- 
+  data.frame(
+    mtry = 3848,
+    min_n = 27,
+    tree_depth = 2,
+    learn_rate = 0.0904 
+  )
+
+best_model_manual <- 
+  boost_tree(
+    trees = 300, 
+    learn_rate = best_params_manual$learn_rate,
+    tree_depth = best_params_manual$tree_depth, 
+    min_n = best_params_manual$min_n,
+    loss_reduction = 0, 
+    mtry = best_params_manual$mtry
+  ) %>%
+  set_engine(engine = "lightgbm") %>%
+  set_mode(mode = "classification") %>% 
+  translate()
+
+
+# Once the final model is determined, the entire training set is used for the last fit.
+testing <- last_fit(best_model_manual, full_dataset, split = dataset_split) 
 
 testing %>% 
   collect_metrics()
@@ -253,35 +277,106 @@ violin_plot = ggplot(df_predicciones, aes(x=AS, y=.pred_1, group=AS, fill=factor
 
 violin_plot
 
+# Modelo final----
+
+# El fit en todo el dataset de entrenamiento esta en testing
+
+final_lgbm_model <- 
+  boost_tree(
+    trees = 300, 
+    learn_rate = best_params_manual$learn_rate,
+    tree_depth = best_params_manual$tree_depth, 
+    min_n = best_params_manual$min_n,
+    loss_reduction = 0, 
+    mtry = best_params_manual$mtry
+  ) %>%
+  set_engine(engine = "lightgbm") %>%
+  set_mode(mode = "classification")
+
+final_wflow <- 
+  workflow() %>% 
+  add_formula(AS ~ .) %>% 
+  add_model(final_lgbm_model) 
+
+final_fit <- final_wflow %>%  fit(data = train_data_total)
 
 
-# Importancia de variables enlatada
-
-# opc A
-testing %>%
-  extract_workflow() #%>%
-  # vip(geom = "point") + 
-  # labs(title = "Random forest variable importance") 
-
-# Opc B
-explainer_model <- 
+# Feature importance----
+simple_explainer <- 
   explain_tidymodels(
-  last_fit, 
-  data = train_data_total, 
-  y = train_data_total$AS,
-  label = "feature importance?",
-  verbose = FALSE
+    final_fit,
+    data = dplyr::select(train_data_total, -AS),
+    y = as.integer(train_data_total$AS),
+    label = "simple lgbm explainer",
+    verbose = FALSE
 )
 
-# SHAP
+simple_global_explainer <- 
+  simple_explainer %>% 
+  model_parts() #Esta función genera explicaciones globales
+
+# Esto no funciona y no entiendo por qué
+# Es este error https://www.statology.org/contrasts-applied-to-factors-with-2-or-more-levels/
+# Pero veo mas de 3 levels en todas las variables
+simple_breakdown <- predict_parts(
+  explainer = simple_explainer, 
+  new_observation = test_data[1,] %>%  select(-AS))
+
+# Función tomada de https://www.tmwr.org/explain.html
+ggplot_imp <- function(...) {
+  obj <- list(...)
+  metric_name <- attr(obj[[1]], "loss_name")
+  metric_lab <- paste(metric_name, 
+                      "after permutations\n(higher indicates more important)")
+  
+  full_vip <- bind_rows(obj) %>%
+    filter(variable != "_baseline_")
+  
+  perm_vals <- full_vip %>% 
+    filter(variable == "_full_model_") %>% 
+    group_by(label) %>% 
+    summarise(dropout_loss = mean(dropout_loss))
+  
+  p <- full_vip %>%
+    filter(variable != "_full_model_") %>% 
+    mutate(variable = fct_reorder(variable, dropout_loss)) %>%
+    ggplot(aes(dropout_loss, variable)) 
+  if(length(obj) > 1) {
+    p <- p + 
+      facet_wrap(vars(label)) +
+      geom_vline(data = perm_vals, aes(xintercept = dropout_loss, color = label),
+                 size = 1.4, lty = 2, alpha = 0.7) +
+      geom_boxplot(aes(color = label, fill = label), alpha = 0.2)
+  } else {
+    p <- p + 
+      geom_vline(data = perm_vals, aes(xintercept = dropout_loss),
+                 size = 1.4, lty = 2, alpha = 0.7) +
+      geom_boxplot(fill = "#91CBD765", alpha = 0.4)
+    
+  }
+  p +
+    theme(legend.position = "none") +
+    labs(x = metric_lab, 
+         y = NULL,  fill = NULL,  color = NULL)
+}
+
+ggplot_imp(simple_explainer)
+
+
+
+
+
+# SHAP----
 # https://community.rstudio.com/t/shap-values-with-tidymodels/147217
 # https://www.tmwr.org/explain.html
 
-# Opc B
-shap <- 
+test_case <- test_data[1,]
+
+shap_values <-  
   predict_parts(
-    explainer = explainer_model, 
-    new_observation = test_data, 
+    explainer = simple_explainer, 
+    new_observation = test_case, 
     type = "shap",
     B = 20
   )
+
